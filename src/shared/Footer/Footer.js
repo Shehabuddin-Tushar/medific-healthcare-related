4a401a1afa0a7923846130b0f64c078ccb562b29
Tushar <freelancerrtushar@gmail.com>
2021-10-19 17:18:21 +0600

    some changes

--- src/shared/Footer/Footer.js
+++ src/shared/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Footer.css'
 function Footer() {
     return (
@@ -8,17 +9,17 @@ function Footer() {
                 <div className="quicklinks flex">
                 <div className="first-links">
                    <ul>
-                       <li><a href=""><i class="fas fa-angle-double-right"></i> Home</a></li>
-                       <li><a href=""><i class="fas fa-angle-double-right"></i> Our partnars</a></li>
-                       <li><a href=""><i class="fas fa-angle-double-right"></i> our service</a></li>
-                       <li><a href=""><i class="fas fa-angle-double-right"></i> Contact us</a></li>
+                       <li><Link to="/home"><i class="fas fa-angle-double-right"></i> Home</Link></li>
+                       <li><Link to="/about"><i class="fas fa-angle-double-right"></i> Our partnars</Link></li>
+                       <li><Link to="/service"><i class="fas fa-angle-double-right"></i> our service</Link></li>
+                       <li><Link to="/contact"><i class="fas fa-angle-double-right"></i> Contact us</Link></li>
                    </ul>
                </div>
                <div className="second-links">
                     <ul>
-                       <li><a href=""><i class="fas fa-angle-double-right"></i> Our patient</a></li>
-                       <li><a href=""><i class="fas fa-angle-double-right"></i> Our People</a></li>
-                       <li><a href=""><i class="fas fa-angle-double-right"></i> About</a></li>
+                       <li><Link to="/home"><i class="fas fa-angle-double-right"></i> Our patient</Link></li>
+                       <li><Link to="/home"><i class="fas fa-angle-double-right"></i> Our People</Link></li>
+                       <li><Link to="/about"><i class="fas fa-angle-double-right"></i> About</Link></li>
                     </ul> 
                </div>
                 </div>
@@ -26,22 +27,22 @@ function Footer() {
             </div>
             <div className="contactus">
                 <h1 className="text-4xl text-center font-cold mb-3 font-bold">Contact us</h1>
-                 <h3 className="text-3xl mb-3"><a href=""><i class="fas fa-phone-alt"></i> 01857195638</a></h3>
-                 <h2 className="text-3xl mb-3"><a href=""><i class="fas fa-map-marker-alt"></i> View our location</a></h2>
+                 <h3 className="text-3xl mb-3"><Link to="/home"><i class="fas fa-phone-alt"></i> 01857195638</Link></h3>
+                 <h2 className="text-3xl mb-3"><Link to="/contact"><i class="fas fa-map-marker-alt"></i> View our location</Link></h2>
                  <div className="social-link">
-                     <a href=""><i class="fab fa-twitter"></i></a>
-                     <a href=""><i class="fab fa-facebook"></i></a>
-                     <a href=""><i class="fab fa-youtube"></i></a>
+                     <Link to="https://twitter.com/"><i class="fab fa-twitter"></i></Link>
+                     <Link to="https://www.facebook.com/"><i class="fab fa-facebook"></i></Link>
+                     <Link to="https://www.youtube.com/"><i class="fab fa-youtube"></i></Link>
 
                  </div>
             </div>
             <div className="recognition">
                <h1 className="text-4xl mb-3 font-bold">Recognition</h1>
                <div className="recognitoin-image flex">
-                    <a href=""><img src="https://i.postimg.cc/yY2r8GRL/recog1.png"></img></a>
-                    <a href=""><img src="https://i.postimg.cc/q7Tb8Cjn/recog2.png"></img></a>
-                    <a href=""><img src="https://i.postimg.cc/8CmywP0Y/recog3.png"></img></a>
-                    <a href=""><img src="https://i.postimg.cc/DwspJcdn/recog4.png"></img></a>
+                    <Link to="/home"><img src="https://i.postimg.cc/yY2r8GRL/recog1.png"></img></Link>
+                    <Link to="/home"><img src="https://i.postimg.cc/q7Tb8Cjn/recog2.png"></img></Link>
+                    <Link to="/about"><img src="https://i.postimg.cc/8CmywP0Y/recog3.png"></img></Link>
+                    <Link to="/contact"><img src="https://i.postimg.cc/DwspJcdn/recog4.png"></img></Link>
                </div>
             </div>
         </div>
--- src/shared/Hader/Hader.js
+++ src/shared/Hader/Hader.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import useFirebase from '../../hooks/Firebasehook';
 import './Hader.css'
 function Hader() {
@@ -37,9 +37,9 @@ function Hader() {
                     <h3>+00 1857193407</h3>
                  </div>
                  <div className="hidden md:flex space-x-6">
-                    <a href=""><i class="fab fa-facebook-f"></i></a>
-                    <a href=""><i class="fab fa-twitter"></i></a>
-                    <a href=""><i class="fab fa-whatsapp"></i></a>
+                    <Link to="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></Link>
+                    <Link to="ps://twitter.com/htt"><i class="fab fa-twitter"></i></Link>
+                    <Link to="https://www.whatsapp.com/"><i class="fab fa-whatsapp"></i></Link>
                  </div>
             </div>
            {/* navbar goes here */}
